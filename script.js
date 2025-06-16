c6201814c2be0d4a4df4e37df4b3d57ca3adb0dc
ANSHARXIRPL <anshar080823@gmail.com>
2025-06-16 11:57:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Debugging dimulai!");
+let x = 10;
+let y = 20;
+let total = x + y;
+console.log("Total:", total);
+
+// Optional JavaScript if you want to add extra behavior
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Carousel siap tampil cantik 💖');
+});
+
+
+
